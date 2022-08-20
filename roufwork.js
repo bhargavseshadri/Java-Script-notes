a2481f093aedf29d0261d537961f6fd0e3610a13
bhargav seshadri <bhargavseshadri2389@gmail.com>
2022-08-20 13:18:32 +0530

    first commit

---
+++ roufwork.js
@@ -0,0 +1,24 @@
+
+/*var isEven = (Element) => {
+    return Element = Element + 2;
+};
+
+var result = isEven(10);
+console.log(result);*/
+
+
+/*function addS(name){
+    console.log(name+"s");
+};
+
+addS("bhargav");
+addS("singam");*/
+
+function map() {
+    var array = [];
+
+
+    
+}
+
+
